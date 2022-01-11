e132071eddb44b752affd274b733bf39d09462e4
Manu Reddy <reddybmanohar@gmail.com>
2022-01-11 11:54:47 +0530

    alb pri-alb out & catalogue main

--- Terraform-mutable/main.tf
+++ Terraform-mutable/main.tf
@@ -1,13 +1,13 @@
 module "catalogue" {
-  source = "git::https://github.com/ReddyManu/Terraform-mutable.git//app-module"
-  INSTANCE_TYPE = var.INSTANCE_TYPE
-  ENV = var.ENV
-  OD_INSTANCE_COUNT = var.OD_INSTANCE_COUNT
+  source              = "git::https://github.com/ReddyManu/Terraform-mutable.git//app-module"
+  INSTANCE_TYPE       = var.INSTANCE_TYPE
+  ENV                 = var.ENV
+  OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
   SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
-  COMPONENT = "catalogue"
-  PORT = 8080
-  IS_PRIVATE_LB = true
-  LB_RULE_PRIORITY = 101
+  COMPONENT           = "catalogue"
+  PORT                = 8080
+  IS_PRIVATE_LB       = true
+  LB_RULE_PRIORITY    = 101
 }
 
 output "INSTANCE_IDS" {
